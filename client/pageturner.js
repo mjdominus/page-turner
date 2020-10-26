6dd6d2b15974dbbeaa4a55a1767dfe01d62968ca
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2020-10-26 00:55:45 -0400

    first cut at read-only page turner
    All it does it contact the server, fetch the current page name,
    and store it into a SPAN element

---
+++ client/pageturner.js
@@ -0,0 +1,69 @@
+var pageLocked = true;
+var curPage = "blah";
+var serverURL = "http://127.0.0.1:5000";
+
+var rr = { "waiting": false,
+           "getpage": new XMLHttpRequest(),
+           "delay": 1000,
+           "timeout": null,
+         }
+
+function sendrequest() {
+    console.log("sendrequest", rr)
+    if (rr.waiting) {
+        // already waiting, ignore
+    } else {
+        rr.getpage.addEventListener("load", getCompleted );
+        rr.getpage.open("GET", serverURL + "/get-page");
+        rr.getpage.send();
+        rr.waiting = true
+    }
+}
+
+function getCompleted() {
+    console.log("getCompleted", this, this.responseText);
+    rr.waiting = false
+
+    reply = JSON.parse(this.responseText)
+    rr.action(reply)
+}
+
+rr.schedule_action = function() {
+    console.log("schedule_action", this);
+    if (this.timeout != null) {
+        window.clearInterval(this.timeout)
+    }
+    this.timeout = window.setInterval(sendrequest, this.delay)
+}
+
+rr.STOP = function() {
+    window.clearInterval(this.timeout)
+}
+
+rr.action = function(response) {
+    console.log("in action, response = ", response)
+    update_filenamedisplay(response.page);
+}
+
+
+function STOP() {
+    window.clearInterval(rr.timeout)
+}
+
+function lockPage() {
+    pageLocked = true;
+}
+function unlockPage() {
+    pageLocked = false;
+}
+
+function GETcompleted() {
+    console.log(this.responseText);
+    reply = JSON.parse(this.responseText)
+    console.log(reply);
+    update_filenamedisplay(reply.page);
+}
+
+function update_filenamedisplay(text) {
+    document.getElementById('filenamedisplay').textContent = text;
+}
