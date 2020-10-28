d99d5b22b3fbcd8de8018063af8d4e59b0d2f265
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2020-10-28 16:18:41 -0400

    follower mode is working!

--- client/pageturner.js
+++ client/pageturner.js
@@ -1,21 +1,57 @@
-var rr = { "waiting": false,
-           "getpage": new XMLHttpRequest(),
-           "delay": 1000,
-           "timeout": null,
-           "pageLocked": true,
-           "serverURL": "http://127.0.0.1:5000",
-           "window": window,
-           "pagename": null,
-           "pagenumber": null,
-           "password": null,
-         }
 
-function show(where, what) {
+
+var rr_init = { "waiting": false,
+                "delay": 3000,
+                "timeout": null,
+                "pageLocked": true,
+                "serverURL": "http://127.0.0.1:5000",
+                "pagename": null,
+                "pagenumber": null,
+                "password": null,
+              }
+
+var rr = {};
+var r = {};
+
+window.onload = function () {
+    restoreState()
+
+    if (r.data.password == null) {  // follower
+        r.refresh_elements()
+        r.timeout = window.setInterval(sendrequest, r.data.delay)
+    } else { // leader
+
+    }
+}
+
+function saveState(what) {
+    window.sessionStorage.setItem("saved", JSON.stringify(what))
+}
+
+function restoreState() {
+    var rr
+    console.log("restoring state")
+    saved_json = window.sessionStorage.getItem("saved")
+    if (! saved_json) {
+        console.log("creating new state object")
+        rr = { ...rr_init }
+    } else {
+        console.log("found saved state:", saved_json)
+        rr = JSON.parse(saved_json)
+    }
+    r.data = rr
+    r.getpage = new XMLHttpRequest()
+    stuff_text("debug_rr", JSON.stringify(rr))
+}
+
+function stuff_text(where, what) {
     document.getElementById(where).textContent = what;
 }
 
 function filename_to_number(filename) {
-    const pat = /slide0*(\d+)/
+    //    const pat = /slide0*(\d+)/
+    console.log("to number: ", filename)
+    const pat = /test(\d*)\.html$/
     let g = filename.match(pat)
     if (g == null) {
         return "???"
@@ -24,39 +60,23 @@ function filename_to_number(filename) {
     }
 }
 
-rr.refresh_elements = function() {
-    if (this.pageLocked) {
-        rr.update_phantom_numbers(function(elem) {
+r.refresh_elements = function() {
+    if (this.data.pageLocked) {
+        this.update_phantom_numbers(function(elem) {
             elem.style.backgroundColor = "white"
             elem.style.visibility = "hidden"
         })
-        show("locked_display", "locked")
     } else {
-        rr.update_phantom_numbers(function(elem) {
+        var data = this.data
+        this.update_phantom_numbers(function(elem) {
             elem.style.backgroundColor = "silver"
             elem.style.visibility = "visible"
-            elem.textContent = rr.pagenumber
+            elem.textContent = data.pagenumber
         })
-        show("locked_display", "unlocked")
     }
 }
 
-function phantom_click() {
-    console.log("phantom click")
-    rr.lockPage()
-}
-
-rr.lockPage = function() {
-    this.pageLocked = true
-    this.refresh_elements()
-}
-
-rr.unlockPage = function() {
-    this.pageLocked = false
-    this.refresh_elements()
-}
-
-rr.update_phantom_numbers = function(cb) {
+r.update_phantom_numbers = function(cb) {
     pagenumber_element = document.getElementsByClassName("phantom_number")
     for (i=0; i < pagenumber_element.length; i++) {
         elem = pagenumber_element[i]
@@ -65,86 +85,114 @@ rr.update_phantom_numbers = function(cb) {
     }
 }
 
-rr.schedule_action = function() {
+function phantom_click() {
+    console.log("phantom click")
+    r.lockPage()
+
+    saveState(r.data)
+    window.location.assign(r.data.pagename);
+}
+
+r.lockPage = function() {
+    this.data.pageLocked = true
+    this.refresh_elements()
+}
+
+r.unlockPage = function() {
+    this.data.pageLocked = false
+    this.refresh_elements()
+}
+
+r.schedule_action = function() {
     console.log("schedule_action", this);
     if (this.timeout != null) {
         window.clearInterval(this.timeout)
     }
-    this.timeout = window.setInterval(sendrequest, this.delay)
+    this.timeout = window.setInterval(sendrequest, this.data.delay)
 }
 
-rr.switchpage = function(dir) {
-    if (rr.password == null) {
-        rr.unlockPage()
-    }
+function getlink(dir) {
     links = document.head.getElementsByTagName('link')
     for (i=0; i < links.length; i++) {
         elem = links[i]
-        console.log(i, elem)
         if (elem.rel == dir) {
-            rr.window.location.assign(elem.href);
-            return
+            return elem.href
         }
     }
     alert("Couldn't find link rel=" + dir)
 }
 
-rr.ONCE = function() {
+r.switchpage = function(dir) {
+    if (this.data.password == null) {
+        this.unlockPage()
+    }
+    saveState(this.data)
+    window.location.assign(getlink(dir));
+}
+
+r.ONCE = function() {
     if (this.timeout != null) {
         window.clearInterval(this.timeout)
     }
     this.timeout = window.setTimeout(sendrequest)
 }
 
-rr.STOP = function() {
+r.STOP = function() {
     window.clearInterval(this.timeout)
 }
 
 function sendrequest() {
     console.log("sendrequest", rr)
-    if (rr.waiting) {
+    if (r.waiting) {
         // already waiting, ignore
     } else {
-        rr.getpage.addEventListener("load", getCompleted );
-        rr.getpage.open("GET", rr.serverURL + "/get-page");
-        rr.getpage.send();
-        rr.waiting = true
+        r.getpage.addEventListener("load", getCompleted );
+        r.getpage.open("GET", r.data.serverURL + "/get-page");
+        r.getpage.send();
+        r.waiting = true
     }
 }
 
 function getCompleted() {
     console.log("getCompleted", this, this.responseText);
-    rr.waiting = false
-
     reply = JSON.parse(this.responseText)
-    rr.action(reply)
+    r.action(reply)
 }
 
-rr.action = function(response) {
-    console.log("in action, response = ", response)
-    this.pagename = response.page
-    this.pagenumber = filename_to_number(response.page)
-    update_filenamedisplay(response.page);
-    if (rr.pageLocked) {
-        rr.window.location.assign(response.page);
+r.action = function(response) {
+    this.waiting = false
+    newpage = (new URL(response.page, window.location)).href
+    console.log("in action, response = ", response, "; newpage = ", newpage)
+
+    if (this.data.pageLocked) {
+        if (newpage == this.data.pagename) {
+            console.log("Same page, won't reload")
+        } else {
+            this.data.pagename = newpage
+            this.data.pagenumber = filename_to_number(newpage)
+
+            console.log("Assigning new page: old: ", this.data.pagename, " new: ", newpage)
+            saveState(this.data)
+            window.location.assign(newpage);
+        }
     } else {
-        rr.refresh_elements()
+        console.log("not locked, refreshing")
+        this.data.pagename = newpage
+        this.data.pagenumber = filename_to_number(newpage)
+
+        this.refresh_elements()
     }
 }
 
 function STOP() {
-    window.clearInterval(rr.timeout)
+    window.clearInterval(r.data.timeout)
 }
 
 function GETcompleted() {
     console.log(this.responseText);
     reply = JSON.parse(this.responseText)
     console.log(reply);
-    update_filenamedisplay(reply.page);
-}
-
-function update_filenamedisplay(text) {
-    show("filename_display", text)
+    r.action(reply)
 }
 
 window.onkeydown = function(e) {
@@ -155,12 +203,12 @@ window.onkeydown = function(e) {
     case 34: // page down
     case 39: // right arrow
     case 78: // 'n'
-        rr.switchpage("next")
+        r.switchpage("next")
         break;
     case 37: // left arrow
     case 33: // page up
     case 80: // 'p'
-        rr.switchpage("previous")
+        r.switchpage("previous")
         break;
     }
     return true;
